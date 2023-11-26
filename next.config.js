8127a604ace265dd126becb6c5bd9e8156fe446a
Rahul Rai <rrai06125@gmail.com>
2023-11-26 13:45:19 +0530

    Add Changes in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    typescript: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
     experimental: {
         serverActions: {
             bodySizeLimit: '2mb',
